1af59df4fc20fc686978cace64bb5aef2fea09be
Nayanexx <holanda.nayana@gmail,com>
2017-07-25 14:47:18 -0300

    adding support to angular-bootstrap

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,11 @@
-var app = angular.module('nikeStoreApp',[]);
-    app.controller('carouselController', function($scope, $http) {
-    	$http.get('http://www.raphaelfabeni.com.br/rv/data.json').
-  			success(function(data) {
-				$scope.response = data;
-        var arrays = data["best-sellers"]
-        console.log(arrays);
-        console.log(arrays[1]["high-top"]);
-        
-
-			})
-			.error(function(data) {
-                    alert(data);
-                    console.log('Error: ' + data);
-            });
-     });
+var app = angular.module('nikeStoreApp',['ui.bootstrap']);
+app.controller('carouselController', function($scope, $http) {
+    $http.get('http://www.raphaelfabeni.com.br/rv/data.json')
+  	    .success(function(data) {
+				    $scope.response = data;
+  			})
+			 .error(function(data) {
+            alert(data);
+            console.log('Error: ' + data);
+        });
+});
