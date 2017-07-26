587d7540a79891e4249271f5a809e2f5927589ac
Nayanexx <holanda.nayana@gmail,com>
2017-07-26 12:44:41 -0300

    building carousel

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,21 @@
 var app = angular.module('nikeStoreApp',['ui.bootstrap']);
 app.controller('carouselController', function($scope, $http) {
-    $http.get('http://www.raphaelfabeni.com.br/rv/data.json')
-  	    .success(function(data) {
-				    $scope.response = data;
-  			})
-			 .error(function(data) {
-            alert(data);
-            console.log('Error: ' + data);
-        });
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    var slides = [];
+    
+    
+        $http.get('http://www.raphaelfabeni.com.br/rv/data.json')
+            .success(function(data) {
+                slides = $scope.slides = data;
+                console.log(data["best-sellers"][1].image);
+            })
+           .error(function(data) {
+                alert(data);
+                console.log('Error: ' + data);
+            });
+
+
+    
 });
